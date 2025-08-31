3ceec8b44abf37f1fd4de6d6d8cb5c8d4b61f909
yukion614 <34601876+yukion614@users.noreply.github.com>
2025-08-31 15:51:26 +0800

    新增隱藏對話功能 和修改讀取對話的狀態

--- src/routes/message.ts
+++ src/routes/message.ts
@@ -30,10 +30,36 @@ router.post("/add", async (req: Request, res: Response) => {
 
 //讀取對話訊息
 router.get("/get_all_messages", async (req: Request, res: Response) => {
-  const result = await prisma.message.findMany();
+  const result = await prisma.message.findMany({
+    where: {
+      status: 1,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
   console.log(result);
 
   res.status(200).json({ result });
+});
+
+//隱藏收回對話
+router.post("/remove_messages", async (req: Request, res: Response) => {
+  const { authorId, messageId } = req.body;
+  console.log({ authorId, messageId });
+
+  try {
+    const result = await prisma.message.update({
+      where: {
+        id: Number(messageId), //訊息的id
+        authorId: Number(authorId), //使用者的id
+      },
+      data: { status: 0 },
+    });
+    res.status(200).json({ message: "刪除成功" });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 export default router;
